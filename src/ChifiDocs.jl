e261c7dcce15ded5faa6b8758beb879a86df31e8
emmy <52672675+emmaccode@users.noreply.github.com>
2024-05-23 04:34:36 -0400

    more toolips back-end :)

--- src/ChifiDocs.jl
+++ src/ChifiDocs.jl
@@ -8,11 +8,49 @@ logger = Toolips.Logger()
 session = Session(["/"])
 
 mutable struct DocModule
-    mod::module
+    mod::Module
     color::String
     path::String
     names::Dict{String, Component{<:Any}}
+    DocModule(mod::Module, color::String, path::String) = begin
+        names = Dict{String, Component{<:Any}}()
+        new(mod, color, path, names)
+    end
 end
+
+function generate_menu(mods::Vector{DocModule})
+    menuholder = div("mainmenu", align = "center", 
+    children = [begin
+        mdiv = div(string(menu_mod.mod), text = "$(menu_mod.mod)")
+        style!(mdiv, "background-color" => menu_mod.color, 
+        "color" => "white", "font-size" => 14pt, "padding" => 10px)
+        mdiv
+    end for menu_mod in mods])
+    style!(menuholder, "display" => "inline-block")
+    menuholder::Component{:div}
+end
+
+abstract type AbstractDocClient end
+
+mutable struct DocClient <: AbstractDocClient
+    key::String
+end
+
+getindex(dc::Vector{<:AbstractDocClient}, ref::String) = begin
+    findfirst(cl::AbstractDocClient -> cl.key == ref, dc)
+end
+
+mutable struct ClientDocLoader <: Toolips.AbstractExtension
+    docmods::Vector{DocModule}
+    client_keys::Dict{String, String}
+    clients::Vector{DocClient}
+end
+
+
+function generate_tabbar(c::Toolips.AbstractConnection)
+    
+end
+
 
 doc_mods = Vector{DocModule}()
 
@@ -21,36 +59,12 @@ hover_styles = ("border-radius" => 2px, "border" => "1px solid #333333",
 "position" => "absolute", "z-index" => 5, "background-color" => "white")
 
 
-function generate_menu(mods::Vector{DocModule})
-    [begin
-    
-    end for menu_mod in mods]
-end
+menu = generate_menu(doc_mods)
+
+
 
 function home(c::Toolips.AbstractConnection)
-    mainbod = body("main")
-    curs = Components.cursor("curs")
-    on(c, hovera, "click") do cm::ComponentModifier
-        curs = cm[curs]
-        x, y = curs["x"], curs["y"]
-        style!(hoverdata, "top" => y, "left" => x)
-        append!(cm, mainbod, hoverdata)
-    end
-    hovera2 = a("testa", text = "hoverdata2")
-    on(c, hovera2, "click") do cm::ComponentModifier
-        curs = cm[curs]
-        x, y = curs["x"], curs["y"]
-        style!(hoverdata, "top" => y, "left" => x)
-        append!(cm, mainbod, hoverdata)
-    end
-    on(c, mainbod, "click") do cm::ComponentModifier
-        curs = cm[curs]
-        x, y = curs["x"], curs["y"]
-        style!(hoverdata, "top" => y, "left" => x)
-        append!(cm, mainbod, hoverdata)
-    end
-    push!(mainbod, curs, hovera, hovera2)
-    write!(c, mainbod)
+    write!(c, menu)
 end
 
 main = route(home, "/")
