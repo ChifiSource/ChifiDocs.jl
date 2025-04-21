c78ef22f8cb7e847818025c741f4435277a0a228
emmy <52672675+emmaccode@users.noreply.github.com>
2025-04-20 18:21:23 -0600

    added several new packages to configuration

--- dev.jl
+++ dev.jl
@@ -2,4 +2,4 @@ using Pkg; Pkg.activate(".")
 using Revise
 using ChifiDocs
 
-toolips_process = ChifiDocs.start_project("192.168.1.10":8000)
+toolips_process = ChifiDocs.start_project("192.168.1.28":8000)
