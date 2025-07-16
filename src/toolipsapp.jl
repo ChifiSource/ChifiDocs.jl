f6b1c2c365690e3c0cdc3c2d80ba4a8e09803076
emmy <52672675+emmaccode@users.noreply.github.com>
2025-07-16 11:52:20 -0600

    more large toolips app updates

--- src/toolipsapp.jl
+++ src/toolipsapp.jl
@@ -7,11 +7,10 @@ end
 include("toolipsapp/app main.jl")
 include("toolipsapp/emmy's app.jl")
 
-TOOLIPS_APPS = CarouselArray([ToolipsApp(toolipsapp_main, "app main.jl", "A simple `Toolips` landing page."), 
+TOOLIPS_APPS = CarouselArray([ToolipsApp(toolipsapp_main, "app main", "A simple landing page app for toolips app."), 
     ToolipsApp(EmmysApp.main, "emmy's app", "A simple Toolips + ToolipsSession 'number-guesser' sample app.")])
 
 toolips_app = route("/toolips/app") do c::Connection
-    position = 1
     tl_topbar = style("a.topbar", "color" => "white", "font-weight" => "bold", "cursor" => "pointer", "border-left" => "3px solid #36223d", 
         "font-weight" => "bold", "font-size" => 25pt, "padding" => 2percent, "transition" => 750ms)
     tl_topbar:"hover":["transform" => scale(1.01), "border-left" => "6px solid #7d3f91"]
@@ -23,13 +22,60 @@ toolips_app = route("/toolips/app") do c::Connection
     topbar = div("topbar", align = "left", children = [back_wrap, tlapp_wrap, srclnk])
     style!(topbar, "position" => "absolute", "top" => 0percent, "left" => 0percent, "background-color" => "#432f61", "width" => 100percent, "display" => "inline-block", 
         "height" => 3percent, "overflow" => "hidden")
-    new_frame = iframe("mainframe", src = TOOLIPS_APPS[position].route.path, width = 90percent, height = 90percent)
-
-    gallery = div("gallery", children = new_frame)
-    style!(gallery, "position" => "absolute", "width" => 100percent, "height" => 95percent, "padding" => 2percent, "top" => 3percent, "left" => 0percent, 
-        "background-color" => "#2a272b", "border-top" => "5px solid #36223d")
+    gallery = build_app_window(c, 1)
     meta = title(text = "toolips app !")
     main_body = body("tlapp", children = [meta, tl_topbar, topbar, gallery])
     style!(main_body, "overflow" => "hidden")
     write!(c, main_body)
+end
+
+function build_app_frame(position::Integer)
+    app = TOOLIPS_APPS[position]
+    new_frame = iframe("mainframe", src = app.route.path, width = 95percent, height = 80percent)
+    style!(new_frame, "display" => "inline-block")
+    new_frame
+end
+function build_app_window(c::AbstractConnection, position::Integer)
+    new_frame = build_app_frame(position)
+    butt_left = span("tlback", text = "<")
+    butt_right = span("tlfor", text = ">")
+    s = ("width" => 1percent, "color" => "white", "font-weight" => "bold", "font-size" => 25pt, 
+        "background-color" => "#2b3c8f", "z-index" => -4, "padding" => 1.1percent, "cursor" => "pointer", 
+        "display" => "inline-block")
+    style!(butt_left, s ...)
+    style!(butt_right, s ...)
+    on(c, butt_left, "click") do cm::ComponentModifier
+        curr_pos = parse(Int64, cm["gallery"]["position"])
+        curr_pos -= 1
+        cm["gallery"] = "position" => string(curr_pos)
+        new_win = build_app_frame(curr_pos)
+        remove!(cm, "mainframe")
+        append!(cm, "gallery", new_win)
+        app = TOOLIPS_APPS[curr_pos]
+        set_text!(cm, "appnameh", app.name)
+        set_text!(cm, "appdesc", app.description)
+    end
+    on(c, butt_right, "click") do cm::ComponentModifier
+        curr_pos = parse(Int64, cm["gallery"]["position"])
+        curr_pos += 1
+        cm["gallery"] = "position" => string(curr_pos)
+        new_win = build_app_frame(curr_pos)
+        remove!(cm, "mainframe")
+        append!(cm, "gallery", new_win)
+        app = TOOLIPS_APPS[curr_pos]
+        set_text!(cm, "appnameh", app.name)
+        set_text!(cm, "appdesc", app.description)
+    end
+    app = TOOLIPS_APPS[position]
+    infoheader = h2("appnameh", text = app.name, style = "color:white;")
+    infodesc = p("appdesc", text = app.description, style = "color:white;")
+    infobox = div("infob", children = [butt_left, butt_right, infoheader, infodesc])
+    gallery = div("gallery", children = [infobox, new_frame], align = "center", position = position)
+    style!(gallery, "position" => "absolute", "width" => 100percent, "height" => 97percent, "top" => 3percent, "left" => 0percent, 
+        "background-color" => "#2a272b", "border-top" => "5px solid #36223d")
+    gallery::Component{:div}
+end
+
+function build_app_source(position::Integer)
+
 end
---
+++ src/toolipsapp/app main.md
@@ -0,0 +1,3 @@
+### welcome to toolips app !
+`ToolipsApp` is an application built to demonstrate the numerous capabilities of `Toolips`. This is a gallery 
+of different websites created entirely with `Toolips`. Use the source button at the top to flip between `source` / the web-view.
